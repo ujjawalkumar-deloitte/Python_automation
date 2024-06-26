12699dcd8ba04252b0c20f8950b8d0f0f0ab1a9c
abhishek-deloitte <abhiskushwaha@deloitte.com>
2024-06-26 14:43:13 +0530

    removed files form main branch

--- testData/locators.py
+++
@@ -1,6 +0,0 @@
-loginXpath = {
-    "click_login" : "//a[normalize-space()='Login or register']",
-    "enter_username" : "//*[@id='loginFrm_loginname']",
-    "enter_password" : "//*[@id='loginFrm_password']",
-    "click_loginbutton" : "//*[@title='Login']"
-}
